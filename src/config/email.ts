eee17ed68417c91dd0f7f09bdbb55e4c3e0b5044
Ayoola Damisile Ayooluwa <damisileayoola@gmail.com>
2026-02-26 10:36:33 +0100

    feat: Update EmailJS template ID
    Replaced the placeholder email template ID with the actual ID for the default contact form template. This ensures that emails sent through the contact form use the correct configuration.

--- src/config/email.ts
+++ src/config/email.ts
@@ -1,5 +1,5 @@
 export const EMAILJS_CONFIG = {
   SERVICE_ID: 'service_9fociu9',
   PUBLIC_KEY: 'Qcm-zHPOWnKZ_NhF2',
-  TEMPLATE_ID: 'template_default', // TODO: Create template in EmailJS and replace this with your Template ID
+  TEMPLATE_ID: 'template_u40ll9b',
 };
